30a05a89db53cdd4c9f3faf9abfd7e340caf2329
Pranav Jain <the.pranav.jain.1@gmail.com>
2025-02-15 18:48:20 -0600

    added shoelace

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'home.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "html", "home.html"));
 });
 
 app.listen(port, () => {
